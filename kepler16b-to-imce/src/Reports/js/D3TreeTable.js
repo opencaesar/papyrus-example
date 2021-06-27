cd90bdd85dec13608411ff9d960c89d9c146ffff
Mohammed Mostafa <mohmiim@gmail.com>
2021-06-27 14:32:51 -0400

    better expand collapse

--- kepler16b-to-imce/src/Reports/js/D3TreeTable.js
+++ kepler16b-to-imce/src/Reports/js/D3TreeTable.js
@@ -7,45 +7,23 @@ function treeTable(data, containerID) {
 	var mainDiv = container.append('div');
 	var columns = data.columns;
 
-	var sections = mainDiv.selectAll('button').data(data.roots).enter().append('button')
-					.attr("type","button")
-					.classed("collapsible", true)
-					.text(function (d) {
+	var sections = mainDiv.selectAll('details').data(data.roots).enter().append('details')
+					.classed("collapsible", true);
+	
+	sections.append('summary').text(function (d) {
 	  			 		return d[columns[1]];
-	  					})
-	  				.append('div')
-	  				.classed("content",true)
-	  				.on("click", toggle);
-	
-	function toggle(d,i){
-		i = d3.select(this);
-		console.log(i);
-	}
-	
-	sections.selectAll('p')
+	  					});
+
+	groups = sections.selectAll('ul').data(function (row) {
+					return  [row];
+	  			}).enter().append('ul').classed("content",true);
+	groups.selectAll('li')
 				.data(function (row) {
 	    			return row.children;
-	  			}).enter().append('p').text(function (d) {
+	  			}).enter().append('li').text(function (d) {
 	  			 	return "        " + d[columns[3]] + " : " + d[columns[2]] ;
-	  		}).style("color","black");
-  			
-	var coll = document.getElementsByClassName("collapsible");
-	var i;
-
-	for (i = 0; i < coll.length; i++) {
-	  	coll[i].addEventListener("click", function() {
-	    	this.classList.toggle("active");
-	    	var content = this.children;
-	    	for (l = 0; l < content.length; l++) {
-		    	if (content[l].style.display === "block") {
-		      		content[l].style.display = "none";
-		    	} else {
-		      		content[l].style.display = "block";
-		    	}
-		    }
-	  	});
-	}
-	  
+	  		}).classed("content",true);
+ 
   	return table;
 }
 
